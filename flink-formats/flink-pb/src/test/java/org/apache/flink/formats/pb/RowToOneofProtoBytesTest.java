0fb0a00a7dde52a1c3f4cff0302b1a4fedee7aa7
maosuhan <maosuhan@bytedance.com>
2020-12-14 20:57:47 +0800

    [flink-pb] fix code style

--- flink-formats/flink-pb/src/test/java/org/apache/flink/formats/pb/RowToOneofProtoBytesTest.java
+++ flink-formats/flink-pb/src/test/java/org/apache/flink/formats/pb/RowToOneofProtoBytesTest.java
@@ -24,7 +24,6 @@ import org.apache.flink.table.data.RowData;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class RowToOneofProtoBytesTest {
