7f11150660e68feb43f2576ed67e5f706cbc688a
maosuhan <maosuhan@bytedance.com>
2020-12-14 20:45:13 +0800

    [flink-pb] add lisense files

--- flink-formats/flink-pb/src/test/java/org/apache/flink/formats/pb/RowToSimpleProtoBytesTest.java
+++ flink-formats/flink-pb/src/test/java/org/apache/flink/formats/pb/RowToSimpleProtoBytesTest.java
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.flink.formats.pb;
 
 import org.apache.flink.formats.pb.serialize.PbRowSerializationSchema;
@@ -24,7 +42,8 @@ public class RowToSimpleProtoBytesTest {
 			0.01,
 			StringData.fromString("hello"),
 			new byte[]{1},
-			StringData.fromString("IMAGES"));
+			StringData.fromString("IMAGES"),
+			1);
 
 		RowType rowType = PbRowTypeInformation.generateRowType(SimpleTest.getDescriptor());
 		row = ProtobufTestHelper.validateRow(row, rowType);
@@ -44,6 +63,7 @@ public class RowToSimpleProtoBytesTest {
 		assertEquals("hello", simpleTest.getF());
 		assertEquals(1, simpleTest.getG().byteAt(0));
 		assertEquals(SimpleTest.Corpus.IMAGES, simpleTest.getH());
+		assertEquals(1, simpleTest.getFAbc7D());
 	}
 
 	@Test
@@ -56,7 +76,8 @@ public class RowToSimpleProtoBytesTest {
 			0.01,
 			StringData.fromString("hello"),
 			null,
-			null);
+			null,
+			1);
 
 		RowType rowtype = PbRowTypeInformation.generateRowType(SimpleTest.getDescriptor());
 		row = ProtobufTestHelper.validateRow(row, rowtype);
