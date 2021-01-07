b0b3a2c506975d416aa35162ae4ddeea9e97a2b4
maosuhan <maosuhan@bytedance.com>
2021-01-07 13:44:40 +0800

    [flink protobuf]fix style

--- flink-formats/flink-protobuf/src/test/java/org/apache/flink/formats/protobuf/RowToMultipleLevelMessageProtoBytesTest.java
+++ flink-formats/flink-protobuf/src/test/java/org/apache/flink/formats/protobuf/RowToMultipleLevelMessageProtoBytesTest.java
@@ -26,6 +26,7 @@ import org.apache.flink.table.types.logical.RowType;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class RowToMultipleLevelMessageProtoBytesTest {
--- flink-formats/flink-protobuf/src/test/java/org/apache/flink/formats/protobuf/RowToOneofProtoBytesTest.java
+++ flink-formats/flink-protobuf/src/test/java/org/apache/flink/formats/protobuf/RowToOneofProtoBytesTest.java
@@ -24,6 +24,7 @@ import org.apache.flink.table.data.RowData;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class RowToOneofProtoBytesTest {
